0b767bb4969f6410c90249dbec927574fc920aca
FlameRays <85276926+FlameRays@users.noreply.github.com>
2023-04-09 18:33:30 +0200

    update choose1.php

--- choose1.php
+++ choose1.php
@@ -8,26 +8,29 @@
    <!-- Bootstrap CSS -->
    <link href="myAssetCss/bootstrap.css" rel="stylesheet" />
 
-    <title>Deciding Page</title>
+    <title>Login Page</title>
   </head>
   <body>
-    <h1>Welcome </h1>
-    <a href="index.html" class="btn btn-danger float-end mb-3">BACK</a>
-
+  
+  <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+  <center>
+    <h1>log in </h1>
+    
     <h4>Please select from the option below </h4>
 
-   <div class="dropdown">
-        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-           Choose Where You Belong
-        </a>
+		<div>
+		<button><a class="" href="log_in_student.php">Student</a></button>
+			<button><a class="" href="log_in_lecturer.php">Lecturer</a></button>
+			<button><a class="" href="log_in_admin.php">Admin</a></button>
+		</div>
+		 <br>
+			<div><button><a class="" href="index.html">back</a></button></div>
+			
 
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <li><a class="dropdown-item" href="log_in_student.php">Student</a></li>
-            <li><a class="dropdown-item" href="log_in_lecturer.php">Lecturer</a></li>
-            <li><a class="dropdown-item" href="log_in_admin.php">Admin</a></li>
-        </ul>
-        </div>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+       </center>
+
+       
+   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
   </body>
